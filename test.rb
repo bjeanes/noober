7e87e6f02f5f7602d8a6e66eddbfa4f905153152
Bodaniel Jeanes <me@bjeanes.com>
2010-06-22 20:07:37 +1000

    Added README for initial ideas and a dep for installing textmate + bundles + helper

--- test.rb
+++
@@ -1,3 +0,0 @@
-dep "super awesome" do
-  
-end
---
+++ textmate.rb
@@ -0,0 +1,17 @@
+dep 'textmate' do
+  requires 'TextMate.app', 'textmate helper', 'textmate bundles'
+end
+
+app 'TextMate.app' do
+  source 'http://download-b.macromates.com/TextMate_1.5.9.dmg'
+end
+
+dep 'textmate helper' do
+  requires 'TextMate.app'
+  met? { which 'mate' }
+  meet { shell "ln -sf '#{app_dir('TextMate.app') / 'Contents/SharedSupport/Support/bin/mate'}' /usr/local/bin/mate" }
+end
+
+dep "textmate bundles" do
+  requires 'TextMate.app'
+end
