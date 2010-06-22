7ab51f0937d54efb6c39150e8f1d505b1f4102e9
Bodaniel Jeanes <me@bjeanes.com>
2010-06-22 22:09:47 +1000

    test

--- dev.rb
+++ dev.rb
@@ -1,5 +1,9 @@
 installer 'iphone sdk' do
   met? { 
+    puts "Sup"
+    p(which 'gcc', 'g++', 'autoconf', 'make', 'ld')
+    p(file_exists? '/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app/Contents/MacOS/iPhone Simulator')
+
     which 'gcc', 'g++', 'autoconf', 'make', 'ld' and
     file_exists? '/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app/Contents/MacOS/iPhone Simulator'
   }
