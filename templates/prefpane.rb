984a02f85d592a68e43e3f92d654d789baafdd63
Bodaniel Jeanes <me@bjeanes.com>
2010-06-28 20:56:06 +1000

    Add prefPane template

---
+++ templates/prefpane.rb
@@ -0,0 +1,26 @@
+meta :prefpane, :for => :osx do
+  accepts_list_for :source
+  accepts_list_for :extra_source
+  
+  template {
+    prepare { setup_source_uris }
+    
+    helper :path do
+      "/Library/PreferencePanes" / name
+    end
+    
+    met? {
+      path.exists? or ("~" / path).exists?
+    }
+  
+    before { shell "mkdir -p \"#{path.parent}\"" }
+    meet {
+      process_sources {|archive|
+        Dir.glob("*.prefPane").map {|entry|
+          log_shell "Installing #{entry}",
+                    %Q{mv -v #{entry} "#{path}"}
+        }
+      }
+    }
+  }
+end
