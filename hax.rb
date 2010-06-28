7502174d819bc3ec77c801a6eb890dbd460921b3
Bodaniel Jeanes <me@bjeanes.com>
2010-06-28 20:23:01 +1000

    Added SIMBL plugins to noober

--- hax.rb
+++ hax.rb
@@ -1,6 +1,15 @@
 dep 'hax' do
+  requires 'simbl_bundles'
+end
+
+dep 'simbl_bundles' do
+  requires %w(Blurminal Visor).map {|bundle| "#{bundle}.bundle"}
 end
 
 simbl_bundle 'Blurminal.bundle' do
   source 'http://github.com/timmfin/Blurminal/raw/master/Blurminal-SL-64bit.bundle.zip'
+end
+
+simbl_bundle 'Visor.bundle' do
+  source 'http://cloud.github.com/downloads/darwin/visor/Visor-2.2-84d1873.zip'
 end
