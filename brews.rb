34566ce5b89ff7aa6f2ccfcfbe2697571beb55cf
Bodaniel Jeanes <me@bjeanes.com>
2010-11-19 11:08:44 +1000

    more brews

--- brews.rb
+++ brews.rb
@@ -1,5 +1,13 @@
 dep 'brews' do
-  requires %w(
-    bash mysql postgresql gist readline wget tree graphviz hub imagemagick sphinx v8
+  brews = %w(
+    mongodb redis mysql postgresql sphinx
+    tree graphviz hub imagemagick wget v8
+    gist readline bash zsh fish ack ffmpeg
+    ec2-api-tools ec2-ami-tools growlnotify
+    git git-flow lame node npm webkit2png
+    bash-completion wireshark wkhtmltopdf
+    nmap nginx postgis redcar tmux
   ).map { |brew| dep("#{brew}.managed") }
+
+  requires ["iphone tools"] + brews
 end
