b79f970d9667a2c07cecbf7df8e719696b330a04
Bodaniel Jeanes <me@bjeanes.com>
2010-11-25 00:53:46 +1000

    Add dep stub for fonts with archive source

--- fonts.rb
+++ fonts.rb
@@ -5,3 +5,8 @@ end
 dep 'inconsolata.font' do
   source 'http://www.levien.com/type/myfonts/Inconsolata.otf'
 end
+
+# TODO: make the font template proccess archives to find fonts
+dep 'bitstream vera.font' do
+  source 'http://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/ttf-bitstream-vera-1.10.tar.bz2'
+end
