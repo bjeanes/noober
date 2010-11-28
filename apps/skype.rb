472086ee027bfe092edf062891f67ca488332af0
Bodaniel Jeanes <me@bjeanes.com>
2010-11-28 10:59:33 +1000

    Skype dropboxed

--- apps/skype.rb
+++ apps/skype.rb
@@ -1,3 +1,14 @@
+dep 'skype' do
+  requires 'Skype.app', 'Skype.dropboxed'
+end
+
 dep 'Skype.app' do
   source 'http://www.skype.com/go/getskype-macosx.dmg'
+end
+
+dep 'Skype.dropboxed' do
+  files %w[
+    ~/Library/Preferences/com.skype.skype.plist
+    ~/Library/Application\ Support/Skype/bojeanes
+  ]
 end
